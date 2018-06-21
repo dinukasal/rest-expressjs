ff372dbf5bcd82a21d345655c97bbcb9f55f35c0
dinuka <dinukasal@gmail.com>
2018-06-21 03:40:02 +0000

    added logger

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
- 
+
+expressLogging = require('express-logging'),
+    logger = require('logops');
+
+app.use(expressLogging(logger));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- 
+
 var routes = require("./routes/routes.js")(app);
- 
+
 var server = app.listen(3000, function () {
     console.log("Listening on port %s...", server.address().port);
 });
